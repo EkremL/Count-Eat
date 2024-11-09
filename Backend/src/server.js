c9924d76537a11cf41fe994677a494b700848f5d
EkremL <ekremlale1907@gmail.com>
2024-11-10 00:24:10 +0300

    .

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -58,7 +58,7 @@ app.use("/admin", requireAuth, adminRoutes);
 app.post("/signup", authController.signup_post);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.listen(port, () => {
