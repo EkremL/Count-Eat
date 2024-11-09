782140846b8fe03b6d9e1a432d0a29d652897361
EkremL <ekremlale1907@gmail.com>
2024-11-09 19:03:41 +0300

    .

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // 1 MB
+    chunkSizeWarningLimit: 3000, // 1 MB
   },
 });
